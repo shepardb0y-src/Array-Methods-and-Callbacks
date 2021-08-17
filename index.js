eb90a821ea27f243eb9f31aa0ae587097704bffe
shepardb0y-src <60862129+shepardb0y-src@users.noreply.github.com>
2021-08-17 17:32:28 -0400

    3-3

--- index.js
+++ index.js
@@ -42,8 +42,11 @@ Use the higher-order function called getYears to do the following:
 2. Receive a callback function getFinals from task 2 
 3. Return an array called years containing all of the years in the getFinals data set*/
 
-function getYears(/* code here */) {
-    /* code here */
+function getYears(data, getfinalscb) {
+    const years = getfinalscb(data).map(function (item) {
+        return item.Year;
+    })
+    return years;
 }
 
 
